3f2118b264a5f11f18f550df18bfebbf64a29ac1
Anthony Ferrara <ircmaxell@gmail.com>
2013-08-14 11:52:10 -0400

    Limit to object only

--- lib/TrueObjectStore/TrueObjectStore.php
+++ lib/TrueObjectStore/TrueObjectStore.php
@@ -39,18 +39,8 @@ class TrueObjectStore implements \Countable, \ArrayAccess, \IteratorAggregate {
     protected function hash($key) {
         if (is_object($key)) {
             return 'o_' . spl_object_hash($key);
-        } elseif (is_array($key)) {
-            return 'a_' . serialize($key);
-        } elseif (is_string($key)) {
-            return 's_' . $key;
-        } elseif (is_int($key)) {
-            return 'i_' . $key;
-        } elseif (is_float($key)) {
-            return 'f_' . $key;
-        } elseif (is_resource($key)) {
-            return 'r_' . $key;
         } else {
-            return 'unknown_' . $key;
+            throw new \InvalidArgumentException('A non-object key was provided!');
         }
     }
 }
